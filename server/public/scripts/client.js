e1b1d110ae4e67d2c06100938fe1a65ecbfd0c63
Ryan Templin <tempjuice@hotmail.com>
2017-08-11 14:28:10 -0500

    added a button to the dom that only adds to N for ready for transfer and added an event listener that logs everytime the ready button is clicked

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,6 +5,10 @@ $( document ).ready( function(){
   // load existing koalas on page load
   getKoalas();
 
+  $('#viewKoalas').on('click','.transferButton', function () {
+    console.log('Update button was clicked...');
+  });
+
   // add koala button click
   $( '#addButton' ).on( 'click', function(){
     console.log( 'in addButton on click' );
@@ -27,6 +31,8 @@ $( document ).ready( function(){
     // call saveKoala with the new obejct
     saveKoala( objectToSend );
   }); //end addButton on click
+
+  
 }); // end doc ready
 
 function getKoalas(){
@@ -65,12 +71,17 @@ $('#viewKoalas').empty();
     var koalaName = $('<td>'+koalas.name+'</td>');
     var koalaAge = $('<td>'+koalas.age+'</td>');
     var koalaGender = $('<td>'+koalas.gender+'</td>');
-    var koalaReady = $('<td>'+koalas.transfer_status+'</td>');
+    if ( koalas.transfer_status == 'Y'){
+      var koalaReady = $('<td>'+koalas.transfer_status+'</td>');      
+    } else {
+      var koalaReady = $('<td>'+koalas.transfer_status+'<button class="transferButton">Ready for Transfer</button></td>');      
+    }
     var koalaNotes = $('<td>'+koalas.notes+'</td>');
 
-    $('#viewKoalas').append('<tr>', koalaName, koalaAge, koalaGender, koalaReady, koalaNotes, '</tr>');
-  }
+    
 
+    $('#viewKoalas').append('<tr>', koalaName, koalaAge, koalaGender, koalaReady, koalaNotes, '</tr>');
+  }// end of for loop
   // $koalaName = $('<td>Susan</td>');
   // $('#viewKoalas').append($koalaName);
 
